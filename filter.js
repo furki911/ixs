c0ef4d30dd9204e84bfe17bd4cfec6ab9c7a1413
furki911 <55999845+furki911@users.noreply.github.com>
2020-12-16 13:35:16 +0530

    v7.0

--- filter.js
+++ filter.js
@@ -1,29 +1,4 @@
-let sortBtn = document.querySelector('.filter-menu').children;
-let sortItem = document.querySelector('.filter-item').children;
-
-for(let i = 0; i < sortBtn.length; i++){
-    sortBtn[i].addEventListener('click', function(){
-        for(let j = 0; j< sortBtn.length; j++){
-            sortBtn[j].classList.remove('current');
-        }
-
-        this.classList.add('current');
-        
-
-        let targetData = this.getAttribute('data-target');
-
-        for(let k = 0; k < sortItem.length; k++){
-            sortItem[k].classList.remove('active');
-            sortItem[k].classList.add('delete');
-
-            if(sortItem[k].getAttribute('data-item') == targetData || targetData == "all"){
-                sortItem[k].classList.remove('delete');
-                sortItem[k].classList.add('active');
-            }
-        }
-    });
-}
-
+// Filter Menu and Filter Item
 
 let sorttBtn = document.querySelector('.what-we-offer__filter-menu').children;
 let sorttItem = document.querySelector('.what-we-offer__item-container').children;
@@ -130,3 +105,56 @@ function prevSlide() {
 $('.slider__next, .go-to-next').on('click', function(){
     nextSlide();
 });
+
+// New Arrivals
+
+let sortBtn = document.querySelector('.filter-menu').children;
+let sortItem = document.querySelector('.filter-item').children;
+
+for(let i = 0; i < sortBtn.length; i++){
+    sortBtn[i].addEventListener('click', function(){
+        for(let j = 0; j< sortBtn.length; j++){
+            sortBtn[j].classList.remove('current');
+        }
+
+        this.classList.add('current');
+        
+
+        let targetData = this.getAttribute('data-target');
+
+        for(let k = 0; k < sortItem.length; k++){
+            sortItem[k].classList.remove('active');
+            sortItem[k].classList.add('delete');
+
+            if(sortItem[k].getAttribute('data-item') == targetData || targetData == "all"){
+                sortItem[k].classList.remove('delete');
+                sortItem[k].classList.add('active');
+            }
+        }
+    });
+}
+
+
+const sortButton =  document.querySelector('.filter-item__menu').children;
+const sortChildren = document.querySelector('.our-skills__content').children;
+
+for(let i = 0; i < sortButton.length; i++) {
+    sortButton[i].addEventListener('click', function(){
+        for(let j = 0; j < sortButton.length; j++) {
+            sortButton[j].classList.remove('current');
+        }
+        this.classList.add('current');
+
+        let targetClass = this.textContent;
+        console.log(targetClass);
+
+        for(let k = 0; k < sortChildren.length; k++) {
+            sortChildren[k].classList.remove('disp-flex');
+
+            if(sortChildren[k].getAttribute('data-item') == targetClass){
+                sortChildren[k].classList.add('disp-flex');
+            }
+        }
+
+    });
+}
